ea26668ee3afe0f80ae731cfc8449aacf26e6595
Astrid Ge <qiutongg@umich.edu>
2024-12-01 19:49:05 -0800

    create show hoot route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -11,6 +11,8 @@ const router = express.Router();
 
 router.use(verifyToken);
 //! put the routes that need rotection underneath
+
+//! GET - /hoots
 router.post('/', async (req, res) => {
     try {
         req.body.author = req.user._id;
@@ -23,5 +25,29 @@ router.post('/', async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+//! GET - /hoots
+router.get('/', async (req, res) => {
+    try {
+      const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+      res.status(200).json(hoots);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+});
+
+//! GET - /hoots/:hootId
+router.get('/:hootId', async (req, res) => {
+    try {
+      const hoot = await Hoot.findById(req.params.hootId).populate('author');
+      res.status(200).json(hoot);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+//! PUT - /hoots/:hootId
 
 module.exports = router;
