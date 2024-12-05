87de333e24d9e5c2539b90ed240db85c87e0eaa8
Astrid Ge <qiutongg@umich.edu>
2024-12-05 15:12:44 -0800

    update get/hoot populate data

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -30,7 +30,7 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
     try {
       const hoots = await Hoot.find({})
-        .populate('author')
+        .populate(['author',"comments.author",])
         .sort({ createdAt: 'desc' });
       res.status(200).json(hoots);
     } catch (error) {
